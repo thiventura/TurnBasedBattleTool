d887b2ce6f93fef8d95d579dec997c10d8272947
Thiago Ventura <thiventura@gmail.com>
2018-09-25 15:45:11 -0400

    fire.js default

--- src/fire.js
+++ src/fire.js
@@ -1,12 +1,9 @@
 import firebase from 'firebase'
 
 var config = {
-    apiKey: "AIzaSyAlY_56t_XqgLDifJYKj2Njjc5i7h499Og",
-    authDomain: "pokechess-65165.firebaseapp.com",
-    databaseURL: "https://pokechess-65165.firebaseio.com",
-    projectId: "pokechess-65165",
-    storageBucket: "pokechess-65165.appspot.com",
-    messagingSenderId: "235711954399"
+    apiKey: "apiKey",
+    authDomain: "projectId.firebaseapp.com",
+    databaseURL: "https://databaseName.firebaseio.com"
 };
 
 var fire = firebase.initializeApp(config);
